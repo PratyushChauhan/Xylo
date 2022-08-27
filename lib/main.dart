b65bbde4aa8a137d0f709ee999b2ad0199227f72
Pratyush Chauhan <pratyushchauhan62@gmail.com>
2022-08-27 21:28:59 +0530

    Corrected asset path name, sound should play now.

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,16 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Text(),
+          child: TextButton(
+            onPressed: () {
+              final player = AudioPlayer();
+              // player.setSource(AssetSource(
+              //     'assets/note1.waw'));
+              player.play(
+                  AssetSource("note1.wav")); // will immediately start playing
+            },
+            child: Center(child: Text("Clicc")),
+          ),
         ),
       ),
     );
